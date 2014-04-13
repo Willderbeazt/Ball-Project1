d38cb0ca4f546e180b4260056f2256381271e6ea
William Warren <will@willwarren.co.uk>
2014-04-13 16:37:17 +0100

    Small changes,
    Audio test &
    Fire test

---
+++ Assets/Scripts/PlayNear.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayNear : MonoBehaviour 
+{
+
+	public Transform player;
+	public float range;
+	public AudioClip sound;
+	
+	bool played;
+	public float timing;
+	//delay is time until the sound can be replayed after starting
+	public float delay = 10;
+ 
+	void Awake()
+	{
+		played = false;
+		timing = Time.time;
+	}
+ 
+	void Update()
+	{
+		if (Vector3.Distance(player.position, transform.position)<range)
+		{
+			if(timing<Time.time)
+			{
+		           played = true;
+		           timing = Time.time + delay;
+			}
+		}
+		
+		if(played)
+		{
+			AudioSource.PlayClipAtPoint(sound, transform.position);
+			played=false;
+		}
+		
+		if(played)
+			audio.loop = false;
+	}
+}
